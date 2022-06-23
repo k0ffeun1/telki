6a0216957b694c9dffbde4d6e7e9111903a193a6
k0ffeun1 <96048125+k0ffeun1@users.noreply.github.com>
2022-06-23 14:24:53 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ new fullpage('#fullpage', {
     scrollHorizontally: true,
     credits: { enabled: false },
     scrollingSpeed: 500,
-    scrollBar: true
+    scrollBar: false,
 });
 
 $(document).ready(function () {
